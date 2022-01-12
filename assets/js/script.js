7f79ff233ca8c04bdd328ea77d2e29a09d3c83b4
SLEEPlESS <rosas_u@ymail.com>
2022-01-11 22:27:24 -0800

    add submit event listener to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var taskToDoE1 = document.querySelector("#tasks-to-do");
 
 // crate function to handle creating the task items
 var createTaskHandler = function() {
+    // this is to prevent the browser from refreshing and removing our task
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className ="task-item";
     listItemE1.textContent = "This is a new task."; 
     taskToDoE1.appendChild(listItemE1);
 };
 
-
-buttonE1.addEventListener("click", createTaskHandler);
+// this is to add event listener to the entire form when it is submited which the button also does because of it type value
+formE1.addEventListener("submit", createTaskHandler);
 
