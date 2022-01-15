8dc7d55eb29934c3b0a5b1034ed1097bc53275e0
SLEEPlESS <rosas_u@ymail.com>
2022-01-14 18:52:06 -0800

    optimized load tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,38 +224,18 @@ localStorage.setItem("tasks", JSON.stringify(tasks));
 // loads creates tasks from saved data in local storage 
 var loadTasks = function() {
 // Gets task items from localStorage.
-var tasks = localStorage.getItem("tasks", JSON.stringify(tasks));
-console.log(tasks);
-// Converts tasks from the string format back into an array of objects.
-tasks = JSON.parse(tasks);
-console.log(tasks);
-// Iterates through a tasks array and creates task elements on the page from it.
-for (i = 0; i < tasks.length; i++) {
- console.log(tasks[i]);
- tasks.setAttribute("id", taskIdCounter);
- var listItemEl = document.createElement("li");
- listItemEl.className = "task-item";
- listItemEl.setAttribute("data-task-id", tasks[i]);
- var taskInfoEl = document.createElement("div");
- taskInfoEl.className = "task-info";
- taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
- taskInfoEl.appendChild(listItemEl);
+  var savedTasks = localStorage.getItem("tasks");
 
- if(tasks[i].status === "to do") {
-  listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-  listItemEl.appendChild(tasksToDoEl);
- } 
- else if (tasks[i].status === "in progress") {
-  listItemEl.querySelector("select[name='status-change']").selectedIndex = 1; 
-  listItemEl.appendChild(tasksInProgressEl);
- }
- else if (tasks[i] === "complete"); {
-  listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-  listItemEl.appendChild(tasksCompletedEl);
- }
- taskIdCounter ++;
- console.log(listItemEl);
-}
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+  // loop through savedTasks array
+  for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[i]);
+  }
 };
 
 // Create a new task
